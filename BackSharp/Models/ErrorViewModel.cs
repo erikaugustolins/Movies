729d4b93d1ec60e5fdcb0a54266c20307457e058
Erik Lins <erikaugustolins@gmail.com>
2022-05-24 17:31:00 -0300

    Create Model Movie

---
+++ BackSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BackSharp/Models/Filme.cs
@@ -0,0 +1,71 @@
+﻿namespace BackSharp.Models
+{
+    public class Filme
+    {
+        private int id;
+        private string name;
+        private string title;
+        private string year;
+        private string debut;
+        private string recipe;
+
+       public int getID() 
+        {
+            return this.id;
+        }
+
+       public string getName() 
+        {
+            return this.name;
+        }
+
+       public void setName(string name) 
+        {
+            this.name = name;
+        }
+
+        public string getTitle()
+        {
+            return this.title;
+        }
+
+        public void setTitle(string title) 
+        {
+            this.title = title;
+        }
+
+        public string getYear()
+        {
+            return this.year;
+        }
+
+        public void setYear(string year)
+        {
+            this.year = year;
+        }
+
+        public string getDebut()
+        {
+            return this.debut;
+        }
+
+        public void setDebut(string debut)
+        {
+            this.debut = debut;
+        }
+
+        public string getRecipe()
+        {
+            return this.recipe;
+        }
+
+        public void setRecipe(string recipe)
+        {
+            this.recipe = recipe;
+        }
+
+
+
+
+    }
+}
